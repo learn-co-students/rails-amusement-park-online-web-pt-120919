30b75f5184a9cc53d92b704c475e7075623c5643
asijaw <asija.w@yahoo.com>
2020-06-01 12:14:59 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+    has_secure_password
+
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    def mood
+        if self.nausea && self.happiness
+            mood = self.happiness - self.nausea
+            mood > 0 ? "happy" : "sad" 
+        end 
+    end 
+
 end
